c3b00aebe698b3a39fc326347f8dd07ac689cb57
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-04-16 12:55:11 +0200

    feat: create component homepage

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,10 @@
+import NavBar from '../components/NavBar';
+
+export default function Homepage() {
+    return (
+        <>
+            <NavBar />
+            <h1>HomePage</h1>
+        </>
+    );
+}
