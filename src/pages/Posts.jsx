8816ed9f03bc18ff64ad8ab2f63acb05115b95cb
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-04-16 12:55:54 +0200

    feat: creat component posts

---
+++ src/pages/Posts.jsx
@@ -0,0 +1,10 @@
+import NavBar from '../components/NavBar';
+
+export default function Posts() {
+    return (
+        <>
+            <NavBar />
+            <h1>Posts</h1>
+        </>
+    );
+}
