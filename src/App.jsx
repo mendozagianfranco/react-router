0cace335ec35883224249c40ccc6543ea383e81e
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-04-16 12:56:09 +0200

    feat: add routing in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Homepage from './pages/Homepage';
+import About from './pages/About';
+import Posts from './pages/Posts';
+
 function App() {
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Homepage />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/posts' element={<Posts />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
