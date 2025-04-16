3be504ce29fdaddfa533fe30144abc9d14b799f9
mendozagianfranco <mendozagianfranco88@gmail.com>
2025-04-16 13:15:00 +0200

    feat: add axios and add post in page with style

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,7 +1,25 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 export default function Posts() {
+    const [postsData, setPostsData] = useState([]);
+
+    const endpoint = 'https://jsonplaceholder.typicode.com/posts';
+
+    useEffect(() => {
+        axios.get(endpoint)
+            .then(res => {
+                setPostsData(res.data);
+            });
+    }, []);
+
     return (
         <>
             <h1>Posts</h1>
+            {postsData.map(post => <div className='card-post' key={post.id}>
+                <h2>{post.title}</h2>
+                <p>{post.body}</p>
+            </div>)}
         </>
     );
 }
